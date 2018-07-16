c6148ba747fc759306e0261922612d9e69b40ea5
Amir Ismagilov <progamailism@gmail.com>
2018-07-16 07:20:02 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,79 @@
-<?php
-
-   echo "Hello,World!";
-   echo "TestTestTest"; 
-?>
+<!doctype html>
+<html>
+   <head>
+       <meta charset="utf-8" />
+       <meta name="descirption" content="TimeZilla - Сервис по работе с датой, временем, заметками."/>
+       <title>TimeZilla - Сервис по работе с датой, временем, заметками.</title>
+       <style>
+           *
+           {
+               padding: 0;
+               margin: 0;
+           }
+           .head
+           {
+               padding: 7px;
+               font-style: Arial, sans-serif;
+               color: #ffffff;
+               background-color: #FFD500;
+           }
+           .block
+           {
+               width: 1000px;
+               margin: 0 auto;
+	   }
+           .loginblock
+	   {
+               width: 200px;
+               background-color: #FFD500;
+	       color: #ffffff;
+               padding-left: 10px;
+               padding: 8px;
+           }
+       </style>
+       <script type="javascript/text">
+           document.all.content.value = 'Hello';
+       </script>
+   </head>
+   <body>
+      <h1 class="head">TimeZilla - Сервис по работе с датой, временем, заметками</h1>
+      <div class="block">
+            </br></br><p><b>TimeZilla</b> - Сервис по работе с датой, временем, заметками</p></br></br>
+            <?php
+                 /* @author:Ismagilov A.Z. @amirismagilov*/
+                 /*
+                 echo "agile</br>";
+		 echo "php</br>";
+		 echo "html5</br>";
+		 echo "css</br>";
+		 echo "name:TimeZilla</br>";
+		 echo "Deploy:service,time-management,notes,network</br>";*/
+                 echo "Hello,World!";
+            ?>
+           <div id="content">test</div>
+	   <h3 class="loginblock">
+                Войти
+	   </h3></br>
+	   <div class="loginenter">
+                <form action="index.php" method="POST">
+                   <label>Введите Логин или email:</label></br>
+                   <input name="login" type="text"></br>
+                   <label>Введите Пароль:</label></br>
+		   <input name="password" type="password"></br>
+		   <input name="submit" type="submit" value="Войти"></br>
+                </form>
+	  </div>
+	  <div id="newstruct">
+              <form action="index.php" method="POST">
+                 <label>Создать заметку:</label></br>
+		 <input name="postname" type="text"></br>
+                 <input name="create" type="submit" value="Создать"></br>
+              </form>
+          </div> 
+           <ul>
+              <li><a href="index.php">Главная</a></li>
+           <ul>
+            
+      </div>
+   </body>
+</html>
